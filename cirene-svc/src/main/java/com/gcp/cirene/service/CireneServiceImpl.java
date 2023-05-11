e89c38085a74ced5abc736a0f3572b8bf0b28709
Ciro Santos <cirosantos@google.com>
2023-05-11 21:22:12 +0000

    changing code

--- cirene-svc/src/main/java/com/gcp/cirene/service/CireneServiceImpl.java
+++ cirene-svc/src/main/java/com/gcp/cirene/service/CireneServiceImpl.java
@@ -29,7 +29,7 @@ public class CireneServiceImpl implements CireneService {
 
             final StringBuilder builder = new StringBuilder();
             builder.append("{");
-                //test    
+                //testdd    
                 builder.append("\"size\": ");
                 builder.append(planetArea);
 
