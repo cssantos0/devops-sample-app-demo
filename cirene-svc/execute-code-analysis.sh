ee54b8982b382038dfbedaab7f2121a336766880
Ciro Santos <cirosantos@google.com>
2023-05-11 20:23:41 +0000

    Adjusting sonar and skaffold

--- cirene-svc/execute-code-analysis.sh
+++ cirene-svc/execute-code-analysis.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-echo "Starting SonarQube analysis..."
+TIMEFORMAT='=> SonarQube analysis done in %0lR'
 
-mvn clean
+time {
+  echo "Starting SonarQube analysis..."
 
-echo "SonarQube analysis finished..."
+  SONAR_ORG=$(gcloud secrets versions access 1 --secret=SONAR_ORG)
+  SONAR_PROJECT=$(gcloud secrets versions access 1 --secret=SONAR_PROJECT)
+  SONAR_TOKEN=$(gcloud secrets versions access 1 --secret=SONAR_TOKEN)
+  SONAR_URL=$(gcloud secrets versions access 1 --secret=SONAR_URL)
+
+  mvn clean
+
+  mvn verify sonar:sonar -Pcoverage \
+    -Dsonar.login=${SONAR_TOKEN} \
+    -Dsonar.host.url=${SONAR_URL} \
+    -Dsonar.organization=${SONAR_ORG} \
+    -Dsonar.projectKey=${SONAR_PROJECT}
+
+  echo "SonarQube analysis finished..."
+}
