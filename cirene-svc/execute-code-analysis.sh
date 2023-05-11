9c7fb5c3abc013bcea28044774d08eb056783fed
Ciro Santos <cirosantos@google.com>
2023-05-11 17:33:22 +0000

    add sonar to build

--- cirene-svc/execute-code-analysis.sh
+++ cirene-svc/execute-code-analysis.sh
@@ -2,10 +2,6 @@
 
 echo "Starting SonarQube analysis..."
 
-mvn clean verify sonar:sonar \
-  -Dsonar.projectKey=GCP-DevOps-Demo-Prj-Java-App \
-  -Dsonar.projectName='GCP-DevOps-Demo-Prj-Java-App' \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=<CHANGE_TO_SONAR_TOKEN>
+mvn clean
 
 echo "SonarQube analysis finished..."
